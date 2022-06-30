64aa351416e95f8254e0c5afbf76773158d6d3ee
Osman Seker <50238194+osmanseker@users.noreply.github.com>
2022-06-30 16:36:19 +0200

    First Upload

---
+++ index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const{Client, Intents} = require("discord.js");
+
+var serverID = "992065837202686033";
+
+const client = new Client({intents:[Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS]});
+
+
+client.on("messageCreate",msg =>{
+    if(msg.content.toLowerCase()==="ping"){
+        msg.reply("Night")
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN)
