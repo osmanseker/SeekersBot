bf7b7c5783c98e1f4cfb844bdf27d7c11ad54260
Osman Seker <50238194+osmanseker@users.noreply.github.com>
2022-07-24 16:18:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 require("dotenv").config();
-const{Client, Intents} = require("discord.js");
-const {MessageEmbed} = require('discord.js');
+const { Client, Intents } = require("discord.js");
+const { MessageEmbed } = require('discord.js');
 
 var serverID = "992065837202686033";
 
-const client = new Client({intents:[Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS]});
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
 
-client.on("ready",()=>{
+client.on("ready", () => {
     console.log("Ready")
+
+    const embed = new MessageEmbed()
+        .setTitle('Rules')
+        .setDescription('These are the rules')
+
+    let channel = client.channels.cache.get(1000765418291609641)
+    channel.send({ embeds: [embed] })
 })
 
-client.on("messageCreate",msg =>{
-    if(msg.content.toLowerCase()==="ping"){
+client.on("messageCreate", msg => {
+    if (msg.content.toLowerCase() === "ping") {
         msg.reply("Night")
     }
-    
-    if(msg.content.toLowerCase()==="hello"){
+
+    if (msg.content.toLowerCase() === "hello") {
         msg.reply("whatup")
     }
 })
 
 
-const embed = new MessageEmbed()
-  .setTitle('Rules')
-  .setDescription('These are the rules')
 
-let channel = client.channels.cache.get(1000765418291609641)
-channel.send({embeds: [embed]})
 
 
 
