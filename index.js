206afd1bfccacfd2bffcf1aff706c5abb9c65be5
Osman Seker <50238194+osmanseker@users.noreply.github.com>
2022-08-14 16:39:18 +0200

    Daily Message
    Succesfully created an embed that will be send daily. The message is for Wordle so the community can play the game and post their results. For the testing it will be send every 10 seconds, but this can be set to every 24 hrs

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
-const { Client, Intents } = require("discord.js");
+const { Client, Intents, MessageEmbed } = require("discord.js");
 const fs = require('fs');
 
 var serverID = "992065837202686033";
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] , partials: ["MESSAGE", "CHANNEL", "REACTION"]});
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS], partials: ["MESSAGE", "CHANNEL", "REACTION"] });
 
 //function to post embeds
 var f = {}
@@ -12,21 +12,35 @@ const functionsFiles = fs.readdirSync('./functions').filter(file => file.endsWit
 for (const file of functionsFiles) {
     const func = require(`./functions/${file}`)
 
-    f[file.replace('.js','')] = func
+    f[file.replace('.js', '')] = func
 }
 
 client.on("ready", () => {
-    console.log("Ready")
+    console.log("SeekerBot is ready")
     f.rules(client);
     f.welcome(client);
+
+    //message for wordle
+    setInterval(() => {
+        const embed = new MessageEmbed()
+        .setTitle("It's Wordle Time!\n ")
+        .setDescription("https://www.nytimes.com/games/wordle/index.html \n\nDon't forget to share your results! \n\nWill you be the today's Wordle Wrecker?")
+        .setColor(0xFF0000)
+        .setImage('attachment://wordle.png')
+
+    let channel =  client.channels.cache.get("1007343624675143800")
+    channel.send({ embeds: [embed], files:['./images/wordle.png']})
+
+    },10000);
+
+    
 })
 
-//test message to see if bot is live/reacts
+
 client.on("messageCreate", msg => {
     if (msg.content.toLowerCase() === "hello") {
         msg.reply("hi")
     }
-
 })
 
 
